288338c5f2ec3e135442ba19c7106a0dc1dad44c
gnietto <elcirculoti@gmail.com>
2023-06-13 21:33:34 -0400

    Renombra directorio typos -> typography para clarificar su rol

--- src/Fonts.tsx
+++ src/Fonts.tsx
@@ -9,35 +9,35 @@ function Fonts() {
       font-style: normal;
       font-weight: 400 ;
       font-display: swap;
-      src: url('/typos/MabryPro-Regular.woff2') format('woff2');
+      src: url('/typography/MabryPro-Regular.woff2') format('woff2');
     }
     @font-face {
       font-family: 'MabryPro-Light';
       font-style: normal;
       font-weight: 400 ;
       font-display: swap;
-      src: url('/typos/MabryPro-Light.woff2') format('woff2');
+      src: url('/typography/MabryPro-Light.woff2') format('woff2');
     }
     @font-face {
       font-family: 'MabryPro-Bold';
       font-style: normal;
       font-weight: 400 ;
       font-display: swap;
-      src: url('/typos/MabryPro-Bold.woff2') format('woff2');
+      src: url('/typography/MabryPro-Bold.woff2') format('woff2');
     }
     @font-face {
       font-family: 'TAN-PEARL-Regular';
       font-style: normal;
       font-weight: 400 ;
       font-display: swap;
-      src: url('/typos/TAN-PEARL-Regular.woff2') format('woff2');
+      src: url('/typography/TAN-PEARL-Regular.woff2') format('woff2');
     }
     @font-face {
       font-family: 'Cambon-ExtraLight';
       font-style: normal;
       font-weight: 400 ;
       font-display: swap;
-      src: url('/typos/Cambon-ExtraLight.woff2') format('woff2');
+      src: url('/typography/Cambon-ExtraLight.woff2') format('woff2');
     }
  `}
     />
